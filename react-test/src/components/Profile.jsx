5d2721ddc85bb1037c0210a112be413305532c84
Vadim <vadimtatarnikov@gmail.com>
2021-04-30 00:44:21 +0300

    Maked new componets

---
+++ react-test/src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://naked-science.ru/wp-content/uploads/2018/04/field_image_istock-516189065.jpg" />
+            </div>
+            <div>
+                ava + descriptions
+            </div>
+            <div>
+                My posts
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Profile;
