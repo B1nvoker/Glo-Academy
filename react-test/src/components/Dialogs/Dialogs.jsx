034511447b5d4c0d0d860927afe98ea7cb923413
Vadim <vadimtatarnikov@gmail.com>
2021-05-03 00:43:38 +0300

    Added new styles to dialog

--- react-test/src/components/Dialogs/Dialogs.jsx
+++ react-test/src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
-import headerStyle from './Dialogs.module.css'
+import dialogsStyle from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>Dialogs</div>
+        <div className={dialogsStyle.dialogs}>
+            <div className={dialogsStyle.dialogsItems}>
+                <div className={dialogsStyle.dialog}>
+                    Valera
+                </div>
+                <div className={dialogsStyle.dialog + ' ' + dialogsStyle.active}>
+                    Artem
+                </div>
+                <div className={dialogsStyle.dialog}>
+                    Vova
+                </div>
+                <div className={dialogsStyle.dialog}>
+                    Vadim
+                </div>
+                <div className={dialogsStyle.dialog}>
+                    Misha
+                </div>
+            </div>
+            <div className={dialogsStyle.messages}>
+                <div className={dialogsStyle.message}>Hi</div>
+                <div className={dialogsStyle.message}>How are u?</div>
+                <div className={dialogsStyle.message}>Bye</div>
+            </div>
+        </div>
     );
 }
 
